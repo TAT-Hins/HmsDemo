111ff7983887c75469fbd77d2a4c45864818503b
TAT-Hins <hins011204@qq.com>
2021-01-16 17:29:05 +0800

    Initial commit

---
+++ app/src/main/java/com/hins/hmsdemo/huawei/PushActivity.java
@@ -0,0 +1,46 @@
+package com.hins.hmsdemo.huawei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class PushActivity extends AppCompatActivity {
+
+    private TextView tvToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvToken = findViewById(R.id.tv_log);
+        tvToken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        MyReceiver receiver = new MyReceiver();
+        IntentFilter filter=new IntentFilter();
+        filter.addAction("com.huawei.codelabpush.ON_NEW_TOKEN");
+        PushActivity.this.registerReceiver(receiver,filter);
+    }
+
+    public class MyReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if ("com.huawei.codelabpush.ON_NEW_TOKEN".equals(intent.getAction())) {
+                String token = intent.getStringExtra("token");
+                tvToken.setText(token);
+            }
+        }
+    }
+}
